264449de675167b5f11ab939ece03baef36edca3
Minh Nguyen <Minh.Nguyen@wisetechglobal.com>
2024-12-23 01:10:07 +1100

    update

--- repo/src/views/HomeView.vue
+++ repo/src/views/HomeView.vue
@@ -32,12 +32,18 @@ const showSideBar = computed(() => smAndUp.value)
     <VContainer>
       <VSheet class="pa-4" rounded border>
         <VRow>
-          <VCol cols="12" md="2" :class="smAndDown ? 'd-flex justify-center' : ''">
+          <VCol
+            cols="12"
+            md="3"
+            :class="smAndDown ? 'd-flex justify-center' : ''"
+            order="1"
+            order-md="2"
+          >
             <div>
-              <VImg :src="introPic" rounded="circle" :width="smAndDown ? '100px' : '100%'"></VImg>
+              <VImg :src="introPic" rounded="circle" :width="smAndDown ? '150px' : '100%'"></VImg>
             </div>
           </VCol>
-          <VCol cols="12" md="10">
+          <VCol cols="12" md="9" order="2" order-md="1">
             <div class="text-h5 mb-4">Welcome to my data analytics portfolio!</div>
             <div class="text-justify">
               I am a business professional with over 2 years of experience across diverse
@@ -48,20 +54,73 @@ const showSideBar = computed(() => smAndUp.value)
               solve business challenges, recommend solutions and present these insights through
               clear visualizations to stakeholders.
             </div>
-            <div class="mt-4">
-              <span class="font-weight-bold">Platform and Tools:</span> Spreadsheets, SQL, Python,
-              Pandas, Numpy, Jupyter Notebooks, R Programming, Tableau, Power BI, Big Query, IBM
-              Cognos.
+            <!-- <div class="mt-4">
+              <span class="text-indigo-lighten-1 font-weight-bold">Platform and Tools:</span>
+              Spreadsheets, SQL, Python, Pandas, Numpy, Jupyter Notebooks, R Programming, Tableau,
+              Power BI, Big Query, IBM Cognos.
             </div>
             <div class="mt-4">
-              <span class="font-weight-bold">Background:</span> International trade, business and
-              finance acumen, admin, supply chain and logistic, transportation, operations.
+              <span class="text-indigo-lighten-1 font-weight-bold">Background:</span> International
+              trade, business and finance acumen, admin, supply chain and logistic, transportation,
+              operations.
             </div>
 
             <div class="mt-4">
-              <span class="font-weight-bold">Education:</span> Bachelor and Master Degree in
-              International Business (finished in 2021).
-            </div>
+              <span class="text-indigo-lighten-1 font-weight-bold">Education:</span> Bachelor and
+              Master Degree in International Business (finished in 2021).
+            </div> -->
+          </VCol>
+        </VRow>
+      </VSheet>
+
+      <VSheet class="pa-4 mt-4" rounded border>
+        <div class="text-center mb-4">
+          <div class="text-h5 font-weight-bold mb-2">My personal projects.</div>
+          <div class="text-h6">
+            <span class="text-indigo-lighten-1 font-weight-bold">Tools and techniques: </span>
+            Speadsheets, SQL, Tableau, Python.
+          </div>
+        </div>
+        <VRow>
+          <VCol>
+            <VCard color="indigo-darken-2">
+              <VImg class="bg-grey-lighten-2" src="https://picsum.photos/350/165?random"></VImg>
+              <VCardTitle class="text-h6"> Project 1 </VCardTitle>
+              <VCardText
+                >Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis
+                quis est labore voluptatibus!</VCardText
+              >
+            </VCard>
+          </VCol>
+          <VCol>
+            <VCard color="indigo-darken-2">
+              <VImg class="bg-grey-lighten-2" src="https://picsum.photos/350/165?random"></VImg>
+              <VCardTitle class="text-h6"> Project 2 </VCardTitle>
+              <VCardText
+                >Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis
+                quis est labore voluptatibus!</VCardText
+              >
+            </VCard>
+          </VCol>
+          <VCol>
+            <VCard color="indigo-darken-2">
+              <VImg class="bg-grey-lighten-2" src="https://picsum.photos/350/165?random"></VImg>
+              <VCardTitle class="text-h6"> Project 3 </VCardTitle>
+              <VCardText
+                >Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis
+                quis est labore voluptatibus!</VCardText
+              >
+            </VCard>
+          </VCol>
+          <VCol>
+            <VCard color="indigo-darken-2">
+              <VImg class="bg-grey-lighten-2" src="https://picsum.photos/350/165?random"></VImg>
+              <VCardTitle class="text-h6"> Project 4 </VCardTitle>
+              <VCardText
+                >Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis
+                quis est labore voluptatibus!</VCardText
+              >
+            </VCard>
           </VCol>
         </VRow>
       </VSheet>
