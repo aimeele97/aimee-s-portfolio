3c649dff45bc4b3903558e8822351c48d14213a4
Minh Nguyen <Minh.Nguyen@wisetechglobal.com>
2024-12-22 19:21:45 +1100

    more nav

--- repo/src/views/HomeView.vue
+++ repo/src/views/HomeView.vue
@@ -8,13 +8,36 @@ const showSideBar = computed(() => smAndUp.value)
 
 <template>
   <VNavigationDrawer v-if="showSideBar" :permanent="showSideBar">
-    <VList nav>
+    <VList>
       <VListItem title="Aimee Portfolio"></VListItem>
+      <VDivider></VDivider>
       <VListItem title="Introduction" value="introduction"></VListItem>
-      <VListItem title="Projects" value="projects"></VListItem>
+      <VListGroup>
+        <template #activator="{ props }">
+          <VListItem v-bind="props" title="Projects" value="projects"></VListItem>
+        </template>
+        <VListItem title="Project 1" value="project1"></VListItem>
+        <VListItem title="Project 2" value="project2"></VListItem>
+        <VListItem title="Project 3" value="project3"></VListItem>
+        <VListItem title="Project 4" value="project4"></VListItem>
+      </VListGroup>
       <VListItem title="Contact" value="contact"></VListItem>
     </VList>
   </VNavigationDrawer>
   <VAppBar v-else title="Application bar"></VAppBar>
-  <VMain class="d-flex align-center justify-center" style="min-height: 300px"> Main Content </VMain>
+  <VMain class="d-flex align-center justify-center" style="min-height: 300px">
+    <VContainer>
+      Welcome to my data analytics portfolio! I am a business professional with over 2 years of
+      experience across diverse industries, including retail, factory, freight forwarding, and
+      hotel. During this time, I discovered my true passion for data analytics. This website is a
+      collection of my data analytics projects, which demonstrate how I leverage domain knowledge,
+      online learning, analytical thinking, problem-solving abilities, and technical skills to solve
+      business challenges, recommend solutions and present these insights through clear
+      visualizations to stakeholders. Platform and Tools: Spreadsheets, SQL, Python, Pandas, Numpy,
+      Jupyter Notebooks, R Programming, Tableau, Power BI, Big Query, IBM Cognos. Background:
+      International trade, business and finance acumen, admin, supply chain and logistic,
+      transportation, operations. Education: Bachelor and Master Degree in International Business
+      (finished in 2021).
+    </VContainer>
+  </VMain>
 </template>
