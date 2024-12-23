cb4ffe897a465c05fabe653cc7fac0674a5eae83
Minh Nguyen <Minh.Nguyen@wisetechglobal.com>
2024-12-23 23:25:15 +1100

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import VueRouter from 'unplugin-vue-router/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/aimee-s_portfolio/',
   plugins: [
     VueRouter({
       extensions: ['.vue', '.md'],
