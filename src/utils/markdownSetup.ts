6cd8f0d2b1d9aa4da1b49609a0e8867066de2562
Minh Nguyen <Minh.Nguyen@wisetechglobal.com>
2024-12-25 13:50:34 +1100

    more markdown plugin

--- src/utils/markdownSetup.ts
+++ src/utils/markdownSetup.ts
@@ -1,8 +1,38 @@
 import type { Options } from 'unplugin-vue-markdown/types'
-import MarkdownItAnchor from 'markdown-it-anchor'
 import MarkdownItPrism from 'markdown-it-prism'
+import MarkdownItLinkAttributes from 'markdown-it-link-attributes'
+import MarkdownItAttrs from 'markdown-it-attrs'
+import MarkdownItAnchor from 'markdown-it-anchor'
 
 export const configMarkdown: Options['markdownItSetup'] = (md) => {
-  md.use(MarkdownItAnchor)
   md.use(MarkdownItPrism)
+    .use(MarkdownItLinkAttributes, {
+      matcher(href: string) {
+        return /^https?:\/\//.test(href)
+      },
+      attrs: {
+        target: '_blank',
+        rel: 'noopener',
+      },
+    })
+    .use(MarkdownItAttrs)
+    .use(MarkdownItAnchor, {
+      tabIndex: false,
+      permalink: MarkdownItAnchor.permalink.headerLink(),
+      slugify: (str: unknown) => {
+        let slug = String(str)
+          .trim()
+          .toLowerCase()
+          .replace(/[\s,.[\]{}()/]+/g, '-')
+          .replace(/[^a-z0-9 -]/g, (c) => c.charCodeAt(0).toString(16))
+          .replace(/-{2,}/g, '-')
+          .replace(/^-*|-*$/g, '')
+
+        if (slug.charAt(0).match(/[^a-z]/g)) {
+          slug = 'section-' + slug
+        }
+
+        return encodeURIComponent(slug)
+      },
+    })
 }
