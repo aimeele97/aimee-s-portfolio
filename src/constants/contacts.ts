6fbf65a5737390baac5c5b53fe22973ad3989d65
Minh Nguyen <Minh.Nguyen@wisetechglobal.com>
2025-01-01 19:23:09 +1100

    add contact item link

--- src/constants/contacts.ts
+++ src/constants/contacts.ts
@@ -1,7 +1,7 @@
 export enum ContactType {
   Email = 'Email',
   Github = 'Github',
-  Linkedin = 'Linkedin',
+  Linkedin = 'LinkedIn',
   Tableau = 'Tableau',
 }
 
@@ -11,8 +11,11 @@ export type ContactItem = {
 }
 
 export const contactItems: ContactItem[] = [
-  { contactType: ContactType.Email, link: '' },
-  { contactType: ContactType.Linkedin, link: '' },
-  { contactType: ContactType.Github, link: '' },
-  { contactType: ContactType.Tableau, link: '' },
+  { contactType: ContactType.Email, link: 'mailto:aimee.le2024@gmail.com' },
+  { contactType: ContactType.Linkedin, link: 'https://www.linkedin.com/in/aimeele97' },
+  { contactType: ContactType.Github, link: 'https://github.com/aimeele97' },
+  {
+    contactType: ContactType.Tableau,
+    link: 'https://public.tableau.com/app/profile/aimee.le9707/vizzes',
+  },
 ]
