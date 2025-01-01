71c85129e186eacce20d3f3c2e578218fdce4f52
Minh Nguyen <Minh.Nguyen@wisetechglobal.com>
2025-01-01 17:03:24 +1100

    add static tools

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -9,6 +9,7 @@ export type ProjectNavItem = {
   projectType: ProjectType
   shortName: string
   githubLink: string
+  tools: string[]
 }
 
 export const projects: ProjectNavItem[] = [
@@ -16,20 +17,24 @@ export const projects: ProjectNavItem[] = [
     projectType: ProjectType.Cyclistic,
     shortName: 'Cyclistic Case Study',
     githubLink: 'https://github.com/aimeele97/cyclistic_customer_behaviour_analysis',
+    tools: ['Python', 'MS SQL Server', 'Tableau'],
   },
   {
     projectType: ProjectType.Housing,
     shortName: 'US Housing Values',
     githubLink: 'https://github.com/aimeele97/US_housing_price_analysis',
+    tools: ['Excel'],
   },
   {
     projectType: ProjectType.MintClassics,
     shortName: 'Mint Classics',
     githubLink: 'https://github.com/aimeele97/mintclassics_modelcar_inventory_analysis',
+    tools: ['MySQL'],
   },
   {
     projectType: ProjectType.NetflixSentiment,
     shortName: 'Netflix Sentiment Analysis',
     githubLink: 'https://github.com/aimeele97/netflix_time_series_sentiment_analysis',
+    tools: ['Python'],
   },
 ]
