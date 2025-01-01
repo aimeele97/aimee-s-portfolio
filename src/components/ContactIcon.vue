248f3c446344689439a74f0e735468650a1d025a
Minh Nguyen <Minh.Nguyen@wisetechglobal.com>
2025-01-01 19:27:58 +1100

    add icons

--- src/components/ContactIcon.vue
+++ src/components/ContactIcon.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { ContactType } from '@/constants/contacts'
-import { mdiEmail, mdiGithub, mdiLinkedin } from '@mdi/js'
+import { mdiGithub } from '@mdi/js'
+import GmailIcon from '@/assets/contact/gmail-icon.svg'
+import LinkedinIcon from '@/assets/contact/linkedin-icon.svg'
 import TableauIcon from '@/assets/contact/tableau-icon.svg'
 
 defineProps<{
@@ -10,13 +12,17 @@ defineProps<{
 
 <template>
   <template v-if="contactType == ContactType.Email">
-    <VIcon :icon="mdiEmail"></VIcon>
+    <div class="d-inline-block">
+      <VImg :src="GmailIcon" width="24" height="24"></VImg>
+    </div>
   </template>
   <template v-else-if="contactType == ContactType.Github">
     <VIcon :icon="mdiGithub"></VIcon>
   </template>
   <template v-else-if="contactType == ContactType.Linkedin">
-    <VIcon :icon="mdiLinkedin"></VIcon>
+    <div class="d-inline-block">
+      <VImg :src="LinkedinIcon" width="24" height="24"></VImg>
+    </div>
   </template>
   <template v-else>
     <div class="d-inline-block">
