9cbecef46dafb8ab3c84938be9fc84bba81cfc9b
Minh Nguyen <Minh.Nguyen@wisetechglobal.com>
2025-01-01 16:55:30 +1100

    add github link instead

--- src/components/HomeProjectsList.vue
+++ src/components/HomeProjectsList.vue
@@ -1,34 +1,38 @@
 <script setup lang="ts">
 import { cardColor } from '@/constants/colors'
-import { projects } from '@/constants/projects'
-import { mdiOpenInNew } from '@mdi/js'
+import { projects, type ProjectNavItem } from '@/constants/projects'
+import { mdiGithub, mdiOpenInNew } from '@mdi/js'
+
+const handleProjectItemClick = (projectItem: ProjectNavItem) => {
+  window.open(projectItem.linkToProject, '_blank')
+}
 </script>
 <template>
   <VSheet class="pa-4 mt-4" rounded border>
     <div class="mb-4">
-      <div class="text-h5 font-weight-bold mb-2">My personal projects</div>
+      <div class="text-h5 font-weight-bold mb-2">Personal projects</div>
     </div>
     <VRow>
       <template v-for="project in projects" :key="project.projectType">
         <VCol>
-          <VCard
-            :color="cardColor"
-            height="280"
-            hover
-            link
-            :href="project.linkToProject"
-            target="_blank"
-          >
+          <VCard :color="cardColor" height="280" hover>
             <VImg class="bg-grey-lighten-2" src="https://picsum.photos/350/165?random"></VImg>
-            <VCardItem>
-              <VCardTitle class="text-h6"> {{ project.shortName }} </VCardTitle>
-              <template #append>
-                <VIcon :icon="mdiOpenInNew" size="x-small"></VIcon>
-              </template>
-            </VCardItem>
-            <VCardText class="text-justify">
-              {{ project.shortDesc }}
+            <VCardText>
+              <div class="text-body-1 font-weight-medium">
+                {{ project.shortName }}
+              </div>
             </VCardText>
+            <VCardActions>
+              <VBtn color="grey-darken-3" variant="tonal" @click="handleProjectItemClick(project)">
+                <template #prepend>
+                  <VIcon :icon="mdiGithub"></VIcon>
+                </template>
+                See Github repo
+                <template #append>
+                  <VIcon :icon="mdiOpenInNew"></VIcon>
+                </template>
+              </VBtn>
+            </VCardActions>
           </VCard>
         </VCol>
       </template>
