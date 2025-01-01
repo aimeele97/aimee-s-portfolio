e8881d5a229cb361aa2e71ba8925ec35b0d23570
Minh Nguyen <Minh.Nguyen@wisetechglobal.com>
2025-01-01 20:13:11 +1100

    change intro

--- src/components/HomeIntro.vue
+++ src/components/HomeIntro.vue
@@ -1,14 +1,28 @@
 <template>
   <VSheet class="pa-4 mt-2" rounded border>
-    <div class="text-h4 mb-4">Hi. I'm <span class="font-weight-bold">Aimee Le.</span></div>
-    <div class="text-h5 mb-4">Welcome to my data analytics portfolio!</div>
-    <div class="text-justify">
-      I am a business professional with over 2 years of experience across diverse industries,
-      including retail, factory, freight forwarding, and hotel. During this time, I discovered my
-      true passion for data analytics. This website is a collection of my data analytics projects,
-      which demonstrate how I leverage domain knowledge, online learning, analytical thinking,
-      problem-solving abilities, and technical skills to solve business challenges, recommend
-      solutions and present these insights through clear visualizations to stakeholders.
+    <div class="mb-4">
+      <div class="text-h5 font-weight-bold mb-2">Introduction</div>
+    </div>
+    <div class="text-justify mb-2">
+      Hello! I’m <span class="font-weight-bold">Aimee Le</span>, an enthusiastic
+      <span class="font-weight-bold">Associate Data Analyst </span> with hands-on experience in data
+      cleaning, analysis, and visualization. I specialize in transforming complex datasets into
+      actionable insights that support strategic decision-making and business improvement. With a
+      strong foundation in tools like
+      <span class="font-weight-bold"> SQL, Excel (VBA), Python, and R </span>, I am adept at using
+      statistical and financial models to drive analysis and problem-solving across various business
+      domains.
+    </div>
+
+    <div class="text-justify mb-2">
+      I thrive in collaborative environments, working alongside cross-functional teams to provide
+      clear, data-driven recommendations that help organizations optimize performance. I am always
+      eager to grow my skill set, contribute to impactful projects, and support continuous
+      improvement through data-driven solutions.
+    </div>
+    <div>
+      Take a look at my portfolio to explore the work I've done and the value I can bring to your
+      organization.
     </div>
   </VSheet>
 </template>
